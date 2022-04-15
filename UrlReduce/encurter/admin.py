90a475e91e8fd54b5bd3449584f1ea90b9088999
Vinicius Braga <Vbragadev@gmail.com>
2022-04-15 16:09:52 -0300

    finih project

--- UrlReduce/encurter/admin.py
+++ UrlReduce/encurter/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
 
-from UrlReduce.encurter.models import UrlRedirect
+from UrlReduce.encurter.models import UrlLog, UrlRedirect
 
 @admin.register(UrlRedirect)
 class UrlRedirectAdmin(admin.ModelAdmin):
     list_display = ('destiny', 'slug', 'dated', 'updated',)
     #prepopulated_fields = {'slug': ('destiny',)}
+
+@admin.register(UrlLog)
+class UrlLogAdmin(admin.ModelAdmin):
+    list_display = ('origin', 'host', 'user_agent', 'ip', 'updated', 'url_redirect')
+
+    def has_change_permission(self, request, obj=None) -> bool:
+        return False
+
+
+    def has_delete_permission(self, request, obj=None) -> bool:
+        return False
+
+    def has_add_permission(self, request, obj=None) -> bool:
+        return False
--- UrlReduce/encurter/models.py
+++ UrlReduce/encurter/models.py
@@ -16,3 +16,15 @@ class UrlRedirect(models.Model):
 
     def __str__(self):
         return f'UrlRedirect para {self.destiny}'
+
+
+class UrlLog(models.Model):
+    updated = models.DateTimeField(auto_now=True)
+    origin = models.URLField(max_length=512, null=True, blank=True)
+    user_agent = models.CharField(max_length=512, null=True, blank=True)
+    host = models.CharField(max_length=512, null=True, blank=True)
+    ip = models.GenericIPAddressField(null=True, blank=True)
+    url_redirect = models.ForeignKey(UrlRedirect, models.DO_NOTHING, related_name='Logs')
+
+    def __str__(self):
+        return self.host
